fde7ab39bfe1662c20d39d12f9076d4870c7e43a
yuriykots <setingm@gmail.com>
2016-12-10 15:55:07 -0500

    Added Put and Delete requests.

--- 13-mysql-server.js
+++ 13-mysql-server.js
@@ -8,6 +8,9 @@ express()
   .use(bodyParser.json())
   .get("/users", (req, res, next) =>{
     db("users").then((users) => {
+      if(!users){
+        return res.send(400)
+      }
       res.send(users)
     }, next)
   })
@@ -18,5 +21,26 @@ express()
         res.send(userIds)
       }, next)
   })
-
-  
+  .put("/users", (req, res, next) => {
+    db("users")
+     .where("id", id)
+     .update(req.body)
+     .then((result) =>{
+       if(result === 0){
+         return res.sent(400)
+       }
+       res.send(200);
+     }, next)
+  })
+  .delete("/users", (req, res, next) => {
+    db("users")
+     .where("id", id)
+     .delete(req.body)
+     .then((result) =>{
+       if(result === 0){
+         return res.sent(400)
+       }
+       res.send(200);
+     }, next)
+  })
+  .listen(3000)
