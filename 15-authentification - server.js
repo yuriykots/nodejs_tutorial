c556ddc5b00195b9a57d80a21c3156f8d2216072
yuriykots <setingm@gmail.com>
2016-12-11 15:01:32 -0500

    Learning sessions day 2

--- 15-authentification - server.js
+++ 15-authentification - server.js
@@ -4,6 +4,10 @@ const sessions = require("express-sessions")
 express()
   .use(session({secret: "I love dogs", resave: false, saveUninitialized: false}))
   .get("/", (req, res, next) => {
-    res.send("ok")
+    res.send(req.session)
+  })
+  .get("/set", (req, res, next) => {
+    req.session.name = "yuriy"
+    res.send(req.session)
   })
   .listen(3000)
